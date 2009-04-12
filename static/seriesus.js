8cc90d9d26a5ff25a3456637f4e92188e25a242d
Charlie Groves <charlie.groves@gmail.com>
2009-04-12 15:54:47 -0700

    Add values to series, store in db

--- static/seriesus.js
+++ static/seriesus.js
@@ -1,56 +1,88 @@
-var seriesus = function() {
-    function noop() {
-    }
+function noop() {
+}
 
-    function logError(message) {
-        return function(transaction, error) {
-            console.log(message, error);
-        };
-    }
+function logError(message) {
+    return function(transaction, error) {
+        console.log(message, error);
+    };
+}
 
-    function displaySeries(name, key) {
-        $("#series_title").show();
-        $("#series").append('<li id="' + key + '"><h4>' + name + '</h4><ul>' +
-                            '<form class="add_value" action="/value/add" method="post" class="span-16 last">' +
-                            '<label for="value">Value</label>' +
-                            '<input type="text" name="value" />' +
-                            '<input type="submit" value="Add" />' +
-                            '</form></ul></li>');
-    }
+function displayValue(time, value, series, key) {
+    $("#" + series + " ul form").before('<li id="' + key + '">' + new Date(time) + " - " + value + "</li>");
 
-    var db = openDatabase("seriesus", "1.0", "Seriesus Database", 65536);
+}
+
+function displaySeries(name, key) {
+    $("#series_title").show();
+    $("#series").append('<li id="' + key + '"><h4>' + name + '</h4><ul>' +
+                        '<form class="add_value" action="/value/add" method="post" class="span-16 last">' +
+                        '<label for="value">Value</label>' +
+                        '<input type="text" name="value" />' +
+                        '<input type="submit" value="Add" />' +
+                        '</form></ul></li>');
 
     db.transaction(function(transaction) {
-        transaction.executeSql(
-                "CREATE TABLE IF NOT EXISTS Series(name VARCHAR(500), creator VARCHAR(500), __synced INTEGER, " +
-                "__key VARCHAR(500));", [], noop, logError("Error creating Series"));
-        transaction.executeSql(
-                "CREATE TABLE IF NOT EXISTS Value(time INTEGER, creator VARCHAR(500), value FLOAT, " +
-                "series INTEGER CONSTRAINT fk REFERENCES Series(_ROWID_), __synced INTEGER, __key VARCHAR(500));",
-                [], noop, logError("Error creating Value"));
+        transaction.executeSql("SELECT * from Value WHERE series = ? ORDER BY time;", [key],
+                function(transaction, results) {
+                    var rows = results.rows;
+                    for (var i = 0; i < rows.length; i++) {
+                        var value = rows.item(i);
+                        displayValue(value.time, value.value, value.series, value.__key);
+                    }
+                }, logError("Unable to select series"))
     });
-    $(function() {
-        $("#series_title").hide();
-        $("#add_series").submit(function() {
-            var name = $("#name").attr("value");
-            var key = Math.uuid();
-            db.transaction(function(transaction) {
-                transaction.executeSql("INSERT INTO Series values(?, ?, ?, ?);", [name, "testuser", 0, key],
-                        function(transaction, results) {
-                            console.log("Inserted series", results);
-                        }, logError("Unable to insert series"));
-            });
-            displaySeries(name, key);
-            return false;
-        });
-        db.transaction(function(transaction) {
-            transaction.executeSql("SELECT * from Series;", [], function(transaction, results) {
-                var rows = results.rows;
-                for (var i = 0; i < rows.length; i++) {
-                    displaySeries(rows.item(i).name, rows.item(i).__key);
-                }
-            }, logError("Unable to select series"))
-        });
+    $("#series form:last").submit(function() {
+        var value = new Number($(this).find("[name=value]").attr("value"));
+        var time = new Date().getTime();
+        displayValue(time, value, key, insert("Value", [time, "testemail", value, key]));
+        return false;
     });
-    return {db:db};
-}();
+}
+
+var db = openDatabase("seriesus", "1.0", "Seriesus Database", 65536);
+
+function createTable(tableName, columns) {
+    db.transaction(function(transaction) {
+        var sql = "CREATE TABLE IF NOT EXISTS " + tableName + "(" + columns + ", __synced INTEGER, " +
+                  "__key VARCHAR(500));";
+        transaction.executeSql(sql, [], noop, logError("Error creating " + tableName));
+    });
+}
+function insert(tableName, values) {
+    var key = Math.uuid();
+    db.transaction(function(transaction) {
+        values.push(0);// Never synced on insert
+        values.push(key);
+        var params = [];
+        for (var i = 0; i < values.length; i++) {
+            params.push("?");
+        }
+        params.join(", ")
+        transaction.executeSql("INSERT INTO " + tableName + " VALUES(" + params + ")", values,
+                function(transaction, results) {
+                    console.log("Inserted", tableName, results);
+                }, logError("Unable to insert series"));
+    });
+    return key;
+}
+
+createTable("Series", "name VARCHAR(500), creator VARCHAR(500)");
+createTable("Value", "time INTEGER, creator VARCHAR(500), value FLOAT, " +
+                     "series VARCHAR(500) CONSTRAINT fk REFERENCES Series(__key)");
+
+$(function() {
+    $("#series_title").hide();
+    $("#add_series").submit(function() {
+        var name = $("#name").attr("value");
+        displaySeries(name, insert("Series", [name, "testuser"]));
+        return false;
+    });
+    db.transaction(function(transaction) {
+        transaction.executeSql("SELECT * from Series;", [], function(transaction, results) {
+            var rows = results.rows;
+            for (var i = 0; i < rows.length; i++) {
+                displaySeries(rows.item(i).name, rows.item(i).__key);
+            }
+        }, logError("Unable to select series"))
+    });
+});
