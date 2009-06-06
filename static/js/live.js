6fd985ce9c8ff0d747083ac09ab73f4520551263
Charlie Groves <charlie.groves@gmail.com>
2009-06-06 12:47:06 -0700

    Allow listeners to be removed

--- static/js/live.js
+++ static/js/live.js
@@ -1,35 +1,63 @@
 var live = function(){
+    function ListenerList () {
+        this.listeners = [];
+    }
+    ListenerList.prototype.add = function(listener) {
+        this.listeners.push(listener);
+    }
+    ListenerList.prototype.remove = function(listener) {
+        var i = 0;
+        for (; i < this.listeners.length; i++) {
+            if (this.listeners[i] === listener) {
+                this.listeners.splice(i, 1);
+                return true;
+            }
+        }
+        return false;
+    }
+    ListenerList.prototype.fire = function() {
+       for (var i = 0; i < this.listeners.length; i++) {
+           this.listeners[i].apply(null, arguments);
+        }
+
+    }
+
     function Dict() {
         this.store = {};
-        this.listeners = [];
+        this.listeners = new ListenerList();
     }
 
     Dict.prototype.addPutListener = function(listener) {
-        this.listeners.push(listener);
+        this.listeners.add(listener);
     };
+
+    Dict.prototype.removePutListener = function(listener) {
+        return this.listeners.remove(listener);
+    }
 
     Dict.prototype.put = function(key, val) {
         this.store[key] = val;
-        for (var i = 0; i < this.listeners.length; i++) {
-            this.listeners[i](key, val);
-        }
+        this.listeners.fire(key, val);
     };
     Dict.prototype.get = function(key) {
         return this.store[key];
     };
+
     function List() {
         this.store = [];
-        this.listeners = [];
+        this.listeners = new ListenerList();
     }
     List.prototype.push = function(item) {
         this.store.push(item);
-        for (var i = 0; i < this.listeners.length; i++) {
-            this.listeners[i](item);
-        }
+        this.listeners.fire(item);
     }
     List.prototype.addPushListener = function(listener) {
-        this.listeners.push(listener);
+        this.listeners.add(listener);
     }
+    List.prototype.removePushListener = function(listener) {
+        return this.listeners.remove(listener);
+    }
+
     return {
         Dict:Dict,
         List:List
